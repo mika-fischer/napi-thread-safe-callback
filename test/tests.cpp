8a19b587eb4a868a4fc27a13225edfaf6e03e84e
Mika Fischer <mika.fischer@zoopnet.de>
2017-07-21 00:38:14 +0200

    Add first tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,9 +1,22 @@
 #include <napi.h>
 #include "napi-thread-safe-callback.hpp"
 
-void init(Napi::Env env, Napi::Object exports, Napi::Object module)
-{
+using namespace Napi;
 
+Value test_call(const CallbackInfo& info)
+{
+    auto callback = std::make_shared<ThreadSafeCallback>(info[0].As<Function>());
+    std::thread([callback]
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        callback->call();
+    }).detach();
+    return Boolean::New(info.Env(), true);
+}
+
+void init(Env env, Object exports, Object module)
+{
+    exports["test_call"] = Function::New(env, test_call, "test_call");
 }
 
 NODE_API_MODULE(tests, init);
--- test/tests.js
+++ test/tests.js
@@ -1,8 +1,26 @@
 const assert = require('assert');
-const tests = require('./build/Release/tests');
+const tests = require('./build/Debug/tests');
 
-describe('napi-thread-safe-callback', () => {
-    it('should load tests module', () => {
+describe('napi-thread-safe-callback', function () {
+
+    it('should load tests module', function () {
         assert(tests !== undefined);
-    })
+    });
+
+    describe('test-call', function () {
+        it('should fail when first argument is undefined', function () {
+            assert.throws(() => tests.test_call());
+        });
+            
+        it('should return true and call callback', function (done) {
+            assert(tests.test_call(done) === true);
+        });
+
+        // it('should crash when callback throws', function (done) {
+        //     assert(tests.test_call(() => {
+        //         throw new Error('error');
+        //     }) === true);
+        // });
+
+    });
 });
