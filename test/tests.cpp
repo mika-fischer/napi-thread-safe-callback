a550b8b3ef4cb74d78e9bd5af17ad4954fb6d492
Mika Fischer <mika.fischer@zoopnet.de>
2017-07-24 20:29:51 +0200

    Improve tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -3,7 +3,24 @@
 
 using namespace Napi;
 
-Value test_call(const CallbackInfo& info)
+std::string stringify(const Value& value)
+{
+    auto JSON = value.Env().Global().Get("JSON").As<Napi::Object>();
+    auto stringify = JSON.Get("stringify").As<Napi::Function>();
+    return stringify.Call(JSON, {value}).As<Napi::String>().Utf8Value();
+}
+
+void constructor(const CallbackInfo& info)
+{
+    ThreadSafeCallback(info[0].As<Function>());
+}
+
+void constructor2(const CallbackInfo& info)
+{
+    ThreadSafeCallback(info[0].As<Object>(), info[1].As<Function>());
+}
+
+void call(const CallbackInfo& info)
 {
     auto callback = std::make_shared<ThreadSafeCallback>(info[0].As<Function>());
     std::thread([callback]
@@ -11,12 +28,47 @@ Value test_call(const CallbackInfo& info)
         std::this_thread::sleep_for(std::chrono::milliseconds(5));
         callback->call();
     }).detach();
-    return Boolean::New(info.Env(), true);
 }
+
+void call2(const CallbackInfo& info)
+{
+    auto callback = std::make_shared<ThreadSafeCallback>(info[0], info[1].As<Function>());
+    std::thread([callback]
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        callback->call();
+    }).detach();
+}
+
+void call_args(const CallbackInfo& info)
+{
+    auto callback = std::make_shared<ThreadSafeCallback>(info[0].As<Function>());
+    std::vector<std::string> json_args;
+    for (size_t i=1; i<info.Length(); ++i)
+        json_args.push_back(stringify(info[i]));
+    std::thread([callback, json_args]
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        callback->call([json_args](Napi::Env env, std::vector<napi_value>& args)
+        {
+            auto JSON = env.Global().Get("JSON").As<Napi::Object>();
+            auto parse = JSON.Get("parse").As<Napi::Function>();
+            for (const auto& json_arg : json_args)
+                args.push_back(parse.Call(JSON, {String::New(env, json_arg)}));
+        });
+    }).detach();
+}
+
+#define ADD_TEST(name) \
+    exports[#name] = Function::New(env, name, #name);
 
 void init(Env env, Object exports, Object module)
 {
-    exports["test_call"] = Function::New(env, test_call, "test_call");
+    ADD_TEST(constructor);
+    ADD_TEST(constructor2);
+    ADD_TEST(call);
+    ADD_TEST(call2);
+    ADD_TEST(call_args);
 }
 
 NODE_API_MODULE(tests, init);
--- test/tests.js
+++ test/tests.js
@@ -1,26 +1,115 @@
 const assert = require('assert');
 const tests = require('./build/Debug/tests');
 
-describe('napi-thread-safe-callback', function () {
+describe('napi-thread-safe-callback.hpp', function () {
 
     it('should load tests module', function () {
         assert(tests !== undefined);
     });
 
-    describe('test-call', function () {
-        it('should fail when first argument is undefined', function () {
-            assert.throws(() => tests.test_call());
+    describe('ThreadSafeCallback::ThreadSafeCallback(callback)', function () {
+        it('should fail when called without arguments', function () {
+            assert.throws(() => tests.constructor(), /Callback must be a function/);
         });
-            
-        it('should return true and call callback', function (done) {
-            assert(tests.test_call(done) === true);
+        it('should fail when first argument is undefined', function () {
+            assert.throws(() => tests.constructor(undefined), /Callback must be a function/);
+        });
+        it('should fail when first argument is null', function () {
+            assert.throws(() => tests.constructor(null), /Callback must be a function/);
+        });
+        it('should fail when first argument is boolean', function () {
+            assert.throws(() => tests.constructor(false), /Callback must be a function/);
+        });
+        it('should fail when first argument is number', function () {
+            assert.throws(() => tests.constructor(0), /Callback must be a function/);
+        });
+        it('should fail when first argument is string', function () {
+            assert.throws(() => tests.constructor('foo'), /Callback must be a function/);
+        });
+        it('should fail when first argument is symbol', function () {
+            assert.throws(() => tests.constructor(Symbol()), /Callback must be a function/);
+        });
+        it('should fail when first argument is object', function () {
+            assert.throws(() => tests.constructor({}), /Callback must be a function/);
+        });
+        it('should succeed when first agument is function', function () {
+            assert.doesNotThrow(() => tests.constructor(() => undefined));
+        })
+    })
+
+    describe('ThreadSafeCallback::ThreadSafeCallback(receiver, callback)', function () {
+        it('should fail when called without arguments', function () {
+            assert.throws(() => tests.constructor2(), /Callback receiver must be an object or function/);
+        });
+        it('should fail when called with only receiver', function () {
+            assert.throws(() => tests.constructor2({}), /Callback must be a function/);
+        });
+        it('should fail when first argument is undefined', function () {
+            assert.throws(() => tests.constructor2(undefined, () => {}), /Callback receiver must be an object or function/);
+        });
+        it('should fail when first argument is null', function () {
+            assert.throws(() => tests.constructor2(null, () => {}), /Callback receiver must be an object or function/);
+        });
+        it('should fail when first argument is boolean', function () {
+            assert.throws(() => tests.constructor2(false, () => {}), /Callback receiver must be an object or function/);
+        });
+        it('should succeed when first argument is number', function () {
+            assert.throws(() => tests.constructor2(0, () => {}), /Callback receiver must be an object or function/);
+        });
+        it('should succeed when first argument is string', function () {
+            assert.throws(() => tests.constructor2('foo', () => {}), /Callback receiver must be an object or function/);
+        });
+        it('should succeed when first argument is symbol', function () {
+            assert.throws(() => tests.constructor2(Symbol(), () => {}), /Callback receiver must be an object or function/);
+        });
+        it('should succeed when first argument is object', function () {
+            assert.doesNotThrow(() => tests.constructor2({}, () => {}));
+        });
+        it('should fail when first agument is function', function () {
+            assert.doesNotThrow(() => tests.constructor(() => {}, () => {}));
+        })
+    })
+
+    describe('ThreadSafeCallbacl::call() with callback', function () {
+        it('should call callback', function (done) {
+            tests.call(done);
         });
 
+        // TODO
         // it('should crash when callback throws', function (done) {
-        //     assert(tests.test_call(() => {
+        //     tests.test_call(() => {
         //         throw new Error('error');
-        //     }) === true);
+        //     });
         // });
 
+
     });
+
+    describe('ThreadSafeCallbacl::call() with receiver and callback', function () {
+        it('should call callback with object receiver', function (done) {
+            const receiver = {foo: 'bar'}
+            tests.call2(receiver, function () {
+                assert(this.foo === 'bar');
+                done();
+            });
+        });
+        it('should call callback with function receiver', function (done) {
+            const receiver = () => 'bar';
+            tests.call2(receiver, function () {
+                assert(this() === 'bar');
+                done();
+            });
+        });
+    });
+
+    describe('ThreadSafeCallback::call(arg_func)', function () {
+        it('should call callback with one string argument', function (done) {
+            tests.call_args(function (arg) {
+                assert(arguments.length === 1);
+                assert(arg === 'foo');
+                done();
+            }, 'foo');
+        });
+    });
+
 });
